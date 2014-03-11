7efd711339e8aeb5ca57b0b92ec8332865ebe347
Joshua Ellis <jzellis@gmail.com>
2014-03-11 15:32:44 -0700

    Big UI changes

--- .meteor/local/build/programs/client/app/client/templates/template.about.js
+++ .meteor/local/build/programs/client/app/client/templates/template.about.js
@@ -1,3 +1,3 @@
-(function(){Template.__define__("about",Package.handlebars.Handlebars.json_ast_to_func(["<div class='container'>\n\n<div class='row'>\n\n<div class='col-md-10 col-md-offset-1'>\n\t\t",["#",[[0,"if"],[0,"getImage"],"siteImage"],["<img src='",["{",[[0,"getImage"],"siteImage"]],"' style='float: left; height: 4em'>"]]," <h1>",["{",[[0,"getOption"],"sitename"]],"</h1>\n\t\t<div class='lead'>",["{",[[0,"getOption"],"description"]],"</div>\n\t</div>\n</div>"]));
+(function(){Template.__define__("about",Package.handlebars.Handlebars.json_ast_to_func(["<div class='container'>\n\t\t<div class='col-md-10 col-md-offset-1'>\n<div class='panel panel-default'>\n\t<div class='panel-body'>\n<div class='row'>\n<div class='col-md-12'>\n\n\t\t<h1>",["{",[[0,"getOption"],"sitename"]],"</h1>\n\t\t<hr>\n\t\t<div class='lead'>",["{",[[0,"getOption"],"description"]],"</div>\n\t</div>\n</div>\n</div>\n</div>\n</div>"]));
 
 })();
